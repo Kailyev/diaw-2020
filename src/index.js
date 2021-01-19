1ab254fabbb0fc12b9ddd657c0e69eb89882b106
Kailyev <karlaeef@gmail.com>
2021-01-19 07:47:47 -0600

    materialize working testing with color

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
-// Script de Frontend
-// Importar los estilos
+// Script de Frontend // Importar los estilos
 // import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-
-console.log('Modern JS WORKING!! Thanks babel ...');
-// const show = (message = 'Saludo por defecto') => {
-// alert(message);
-// };
